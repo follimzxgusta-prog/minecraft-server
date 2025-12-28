acd2526f6aa425b7cf556c1f0a259e7fa3452de4
follimzxgusta-prog <follimzxgusta@gmail.com>
2025-12-28 12:07:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-java -Xms1G -Xmx3G -jar server.jar nogui
+
+# Baixa o Paper 1.21 automaticamente
+curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.21/builds/1/downloads/paper-1.21-1.jar
+
+# Inicia o servidor
+java -Xms2G -Xmx6G -jar server.jar nogui
